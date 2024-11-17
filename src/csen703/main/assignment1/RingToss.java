1292e7a30883ae7bf260f842bdc1696067f9c3a2
YousefElbrolosy <yousefelbrolosy@gmail.com>
2024-11-17 16:55:49 +0200

    init: initialised project structure

---
+++ src/csen703/main/assignment1/RingToss.java
@@ -0,0 +1,11 @@
+package csen703.main.assignment1;
+public class RingToss {
+
+    public RingToss() {
+    }
+
+    public static int RingTossGreedy(int [] pegs){
+        return 0;
+    }
+    
+}
